4778fe7b6f084e68ab43a6b08051540db2085488
Jordan Grace <jordan.t.grace@gmail.com>
2020-06-22 09:51:22 -0400

    Added testing chunk from book for testing

--- src/chunk.rs
+++ src/chunk.rs
@@ -177,6 +177,24 @@ mod tests {
         Chunk::new(chunk_type, data)
     }
 
+    fn _testing_chunk_from_book() -> Chunk {
+        let data_length: u32 = 42;
+        let chunk_type = "RuSt".as_bytes();
+        let message_bytes = "This is where your secret message will be!".as_bytes();
+        let crc: u32 = 2882656334;
+
+        let chunk_data: Vec<u8> = data_length
+            .to_be_bytes()
+            .iter()
+            .chain(chunk_type.iter())
+            .chain(message_bytes.iter())
+            .chain(crc.to_be_bytes().iter())
+            .copied()
+            .collect();
+        
+            Chunk::try_from(chunk_data.as_ref()).unwrap()
+    }
+
     #[test]
     fn test_chunk_length() {
         let chunk = testing_chunk();
